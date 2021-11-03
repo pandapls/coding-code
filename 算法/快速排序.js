5ee1b79359ae3dccdeb4d23ccae410f71e67ebca
pandapls <932650719@qq.com>
2021-11-03 22:39:38 +0800

    feat: 完成快速排序

---
+++ 算法/快速排序.js
@@ -0,0 +1,37 @@
+console.log(quickSort([1,3,6,8,4,2]))
+function quickSort(arr,L,R){
+    let len = arr.length;
+    let partitionIndex //基准点下标
+    let left = L ? L : 0
+    let right = R ? R : len-1
+
+    if( left < right ){
+        partitionIndex = partition(arr, left, right)
+        quickSort(arr, left , partitionIndex-1)
+        quickSort(arr, partitionIndex+1 , right)
+
+    }
+    return arr
+}
+
+function partition(arr, left, right){
+    let pivot = left; //选取以left为基准点
+    let index = pivot+1;
+    for(let i = index; i <= right ; i++){
+        // 遍历的元素与基准点相比较 
+        if(arr[i] < arr[pivot]){
+            swap(arr, i, index)
+            index ++;
+        }
+    }
+    swap(arr,pivot,index-1)
+    return index - 1
+
+}
+
+function swap(arr,i,j){
+    console.log(i,j,'i,j')
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
